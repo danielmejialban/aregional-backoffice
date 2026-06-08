b45bb5e3f11c4a893fc46b52a821d033a4172b29
Daniel Albán <danielmejialban@gmail.com>
2026-06-08 14:48:17 +0200

    fix: dashboard.component.ts data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -5,7 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from '@app/services/auth.service';
 import { DepartamentoService } from '@app/services/departamento.service';
 import { VoluntarioService } from '@app/services/voluntario.service';
@@ -48,16 +49,16 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     forkJoin({
-      departamentos: this.departamentoService.getAll(),
-      voluntarios: this.voluntarioService.getAllPaged(0, 1),
-      eventos: this.eventoService.getAllPaged(0, 1),
-      checkIns: this.checkInService.getAllPaged(0, 1)
+      departamentos: this.departamentoService.getAll().pipe(catchError(() => of([]))),
+      voluntarios: this.voluntarioService.getAllPaged(0, 1).pipe(catchError(() => of(null))),
+      eventos: this.eventoService.getAll().pipe(catchError(() => of([]))),
+      checkIns: this.checkInService.getAll().pipe(catchError(() => of([])))
     }).subscribe({
       next: ({ departamentos, voluntarios, eventos, checkIns }) => {
         this.totalDepartamentos = departamentos.length;
-        this.totalVoluntarios = voluntarios.totalElements;
-        this.totalEventos = eventos.totalElements;
-        this.totalCheckIns = checkIns.totalElements;
+        this.totalVoluntarios = voluntarios?.totalElements ?? 0;
+        this.totalEventos = eventos.length;
+        this.totalCheckIns = checkIns.length;
         this.loadingStats = false;
       },
       error: () => { this.loadingStats = false; }
